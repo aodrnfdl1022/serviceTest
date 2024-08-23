2215ff2f56fd384dce73e925d2e6326345549fd8
aodrnfdl <kms991022@naver.com>
2024-08-24 03:07:47 +0900

    123

--- LinkLing-Backend/src/main/java/com/linkling/linklingproject_01/LinkLingProject01Application.java
+++ LinkLing-Backend/src/main/java/com/linkling/linklingproject_01/LinkLingProject01Application.java
@@ -5,6 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@SpringBootApplication
+//@EntityScan({"com.linkling.linklingproject_01.model"})
+//@EnableJpaRepositories({"com.linkling.linklingproject_01.repository"})
+//public class LinkLingProject01Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(LinkLingProject01Application.class, args);
+//    }
+//}
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @EntityScan({"com.linkling.linklingproject_01.model"})
 @EnableJpaRepositories({"com.linkling.linklingproject_01.repository"})
@@ -12,5 +26,10 @@ public class LinkLingProject01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(LinkLingProject01Application.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(Integer.parseInt(System.getProperty("server.port", "8080")));
     }
 }
